c349d1fb977d04ef356feff2c50c149809390e15
Costa <luan.costa@devires.com.br>
2020-05-23 12:25:42 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './home.page';
+import Modal from './modal.component';
+import { ModalProvider } from './modal.context';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ModalProvider>
+      <div className="App">
+        <HomePage />
+        <Modal />
+      </div>
+    </ModalProvider>
   );
 }
 
